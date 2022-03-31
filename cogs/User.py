0bcc29fb69ac918a90206fe21abd63fb20da93ba
lilwooce <kryptstep@gmail.com>
2022-03-31 15:11:48 -0400

    Update User.py

--- cogs/User.py
+++ cogs/User.py
@@ -21,7 +21,8 @@ async def hasAccount(ctx):
     userID = ctx.author.id
     obj = {"f1": "user", "f2": userID}
     result = requests.get(getUser, params=obj, headers={"User-Agent": "XY"})
-    if (int(result.text) == userID):
+    id = result.text.strip('\"')
+    if (id == str(userID)):
         return True
     else:
         await addAccount(ctx)
