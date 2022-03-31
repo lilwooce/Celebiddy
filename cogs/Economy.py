983003ac2192a77d385b13ee15469887fd10b37f
lilwooce <kryptstep@gmail.com>
2022-03-31 19:35:33 -0400

    Update Economy.py

--- cogs/Economy.py
+++ cogs/Economy.py
@@ -52,7 +52,8 @@ class Economy(commands.Cog):
         userID = ctx.author.id
         rn = datetime.now()
         checktime = requests.get(getUser, params={"f1": "workTimer", "f2": userID}, headers={"User-Agent": "XY"})
-        result = checktime.text.strip('\"')[:-7]
+        result = checktime.text.strip('\"')
+        result = result[:-7]
         result = datetime.strptime(result, "%Y-%m-%d %H:%M:%S")
         if (rn >= result):
             balance = requests.get(getUser, params={"f1": "dabloons", "f2": userID}, headers={"User-Agent": "XY"})
