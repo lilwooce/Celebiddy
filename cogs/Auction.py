55e82021d2766aa6e939475c4e96dd84d159b618
lilwooce <kryptstep@gmail.com>
2022-03-30 22:49:52 -0400

    Try again

--- cogs/Auction.py
+++ cogs/Auction.py
@@ -4,21 +4,7 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import requests
 import os
-
-load_dotenv()
-getUser = os.getenv('USER_URL')
-addUser = os.getenv('ADD_USER')
-
-
-async def hasAccount(ctx):
-    userID = ctx.author.id
-    obj = {"f1": "user", "f2": userID}
-    result = requests.get(getUser, params=obj, headers={"User-Agent": "XY"})
-    id = result.text.strip('\"')
-    if (id == userID):
-        return True
-    else:
-        return False
+from User import hasAccount
 
 class Auction(commands.Cog):
     def __init__(self, bot):
