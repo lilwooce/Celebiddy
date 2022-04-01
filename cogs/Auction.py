a59f9e0f687fb2cfb227b2a8b9a59f5478684a27
lilwooce <kryptstep@gmail.com>
2022-03-31 22:56:58 -0400

    Update Auction.py

--- cogs/Auction.py
+++ cogs/Auction.py
@@ -49,8 +49,7 @@ class Auction(commands.Cog):
             embed.add_field(name="Description", value=description, inline=True)
             embed.add_field(name="Occupation", value=occupation, inline=True)
             embed.add_field(name="Attribute", value=attribute, inline=True)
-            print(f"{self.picsLink}{name.replace(' ', '')}{series}.jpg?")
-            embed.set_image(url=f"{self.picsLink}{name}{series}.jpg?")
+            embed.set_image(url="https://cdn.discordapp.com/attachments/743655404583649326/959285067711123527/Zendaya_Coleman_1.jpg")
             await ctx.channel.send(embed=embed)
 
 async def exists(ctx, name, series):
