b695a9adca8cabe29ee4640a83d61701b1e98409
lilwooce <kryptstep@gmail.com>
2022-04-02 02:19:52 -0400

    Update Auction.py

--- cogs/Auction.py
+++ cogs/Auction.py
@@ -68,7 +68,7 @@ async def getInfo(ctx, n, s):
         attribute = requests.get(getCeleb, params={"f1": "attribute", "f2": n}, headers={"User-Agent": "XY"})
         attribute = attribute.text.strip('\"')
         image = requests.get(getCeleb, params={"f1": "image", "f2": n}, headers={"User-Agent": "XY"})
-        image = image.text.strip("\\")
+        image = image.text.strip("\"")
         return description,occupation,attribute,image
 
 def setup(bot):
