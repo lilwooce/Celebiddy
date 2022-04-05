806c9f700c488f570e8fb3957af7e5459d6925c6
lilwooce <kryptstep@gmail.com>
2022-04-05 01:59:00 -0400

    Update User.py

--- cogs/User.py
+++ cogs/User.py
@@ -65,8 +65,7 @@ class User(commands.Cog):
     
     @commands.command(aliases=['v'])
     async def view(self, ctx, series=1, *name):
-        for x in name:
-            name += x + " "
+        name = " ".join(name)
         d,o,a,i,owner = await getInfo(ctx, name, series)
         embed=discord.Embed(title=name, description=f"Works as a(n) {o} \n Owned by <@{owner}>", color=discord.Colour.random())
         embed.add_field(name="Occupation", value=o, inline=True)
