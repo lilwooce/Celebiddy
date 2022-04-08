9cbda635aa0df7a8b78aa0b269508f7746ccfa32
lilwooce <kryptstep@gmail.com>
2022-04-08 14:33:21 -0400

    Create Admin.py

---
+++ Admin.py
@@ -0,0 +1,42 @@
+from ast import alias
+from discord.ext import commands
+import discord
+import math
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import requests
+import os 
+
+load_dotenv()
+getUser = os.getenv('USER_URL')
+updateUser = os.getenv('UPDATE_USER')
+getCeleb = os.getenv('GET_CELEB')
+addCeleb = os.getenv('ADD_CELEB')
+updateCeleb = os.getenv('UPDATE_CELEB')
+addAuction = os.getenv('ADD_AUCTION')
+updateAuction = os.getenv('UPDATE_AUCTION')
+getAuction = os.getenv('GET_AUCTION')
+removeAuction = os.getenv('REMOVE_AUCTION')
+addUser = os.getenv('ADD_USER')
+
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    async def cog_check(self, ctx):
+        owner = "347162620996091904"
+        return owner == ctx.author.id
+
+    @commands.command(aliases=["am"])
+    async def addMoney(self, ctx, user, amount):
+        bal = requests.get(getUser, params={"f1": "dabloons", "f2": user}, headers={"User-Agent": "XY"})
+        bal = bal.text.strip('\"')
+        requests.post(updateUser, data={"f1": "dabloons", "f2": int(bal)+amount, "f3": user.id}, headers={"User-Agent": "XY"})
+    
+    @commands.command(aliases=['co'])
+    async def changeOwnership(self, ctx, name, owner):
+        requests.post(updateCeleb, data={"f1": "owner", "f2": owner, "f3": name}, headers={"User-Agent": "XY"})
+
+
+def setup(bot):
+    bot.add_cog(Admin(bot))
