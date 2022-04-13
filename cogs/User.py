07f27332b89e51ca96919e5ac502085fe4d56666
lilwooce <kryptstep@gmail.com>
2022-04-12 23:18:16 -0400

    Update User.py

--- cogs/User.py
+++ cogs/User.py
@@ -82,7 +82,7 @@ class User(commands.Cog):
         if user is None:
             user = ctx.message.author
         
-        desc = f"Celebrities owned by {user.mention}"
+        desc = f"Celebrities owned by {user.mention} \n \n"
         celebs = requests.get(getCeleb, params={"f1": "name", "f2": ctx.author.id, "f3": 'owner'}, headers=header)
         celebs = celebs.text.strip('\"')
         names = celebs.split(',')
